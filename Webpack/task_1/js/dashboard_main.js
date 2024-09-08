60318213694587120a5df934823da3db6e713853
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2024-09-08 15:07:13 -0300

    js

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -3,13 +3,11 @@ import _ from 'lodash';
 
 let count = 0;
 
-// Function to update the counter and display the number of clicks
 function updateCounter() {
     count += 1;
     $('#count').text(`${count} clicks on the button`);
 }
 
-// Bind the debounced updateCounter function to the button click event
 $(document).ready(function() {
     $('body').append('<p>Holberton Dashboard</p>');
     $('body').append('<p>Dashboard data for the students</p>');
